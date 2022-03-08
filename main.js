a0bb008cdaaefb6759479a1ef47d2ad415b31c6e
BFSmx6 <97541827+BFSmx6@users.noreply.github.com>
2022-03-08 12:13:27 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 
-	name_of_the_student_array = [];
+	nombre_alumno_array = [];
 	
 	function submit()
 	{
         
-        var name_1 = document.getElementById("name_of_the_student_1").value;
-        var name_2 = document.getElementById("name_of_the_student_2").value;
-        var name_3 = document.getElementById("name_of_the_student_3").value;
-        var name_4 = document.getElementById("name_of_the_student_4").value;
+        var nombre_1 = document.getElementById("nombre_alumno_1").value;
+        var nombre_2 = document.getElementById("nombre_alumno_2").value;
+        var nombre_3 = document.getElementById("nombre_alumno_3").value;
+        var nombre_4 = document.getElementById("nombre_alumno_4").value;
 
-        name_of_the_student_array.push(name_1);
-        name_of_the_student_array.push(name_2);
-        name_of_the_student_array.push(name_3);
-        name_of_the_student_array.push(name_4);
+        nombre_alumno_array.push(nombre_1);
+        nombre_alumno_array.push(nombre_2);
+        nombre_alumno_array.push(nombre_3);
+        nombre_alumno_array.push(nombre_4);
 
-		console.log(name_of_the_student_array);
+		console.log(nombre_alumno_array);
 		
-        document.getElementById("display_name").innerHTML = name_of_the_student_array;
+        document.getElementById("ver_nombre").innerHTML = nombre_alumno_array;
 
 	}
 
